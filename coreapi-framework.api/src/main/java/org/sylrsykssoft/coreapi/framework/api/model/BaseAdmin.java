2fb7b3e05dc3ed551e1cb3588e7f9aa99c8964c3
Juan <sylar.sykes@gmail.com>
2019-08-16 16:24:10 +0200

    FIX junit dependencies

--- coreapi-framework.api/src/main/java/org/sylrsykssoft/coreapi/framework/api/model/BaseAdmin.java
+++ coreapi-framework.api/src/main/java/org/sylrsykssoft/coreapi/framework/api/model/BaseAdmin.java
@@ -1,6 +1,5 @@
 package org.sylrsykssoft.coreapi.framework.api.model;
 
-import java.beans.ConstructorProperties;
 import java.time.LocalDateTime;
 
 import javax.persistence.Column;
@@ -11,13 +10,13 @@ import org.springframework.lang.Nullable;
 
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import lombok.Setter;
+import lombok.experimental.SuperBuilder;
 
 /***
  * Entity admin
@@ -28,6 +27,7 @@ import lombok.Setter;
 @MappedSuperclass
 @Where(clause = "removedAt = null")
 @Data
+@SuperBuilder
 @AllArgsConstructor(access = AccessLevel.PUBLIC)
 @NoArgsConstructor(access = AccessLevel.PUBLIC)
 @Setter
@@ -37,34 +37,14 @@ public class BaseAdmin extends Base<Integer> {
 
 	public static final int MAX_LENGTH_NAME = 256;
 	public static final int MAX_LENGTH_DESCRIPTION = 10000;
-	
+
 	@Column(name = "name", nullable = false, unique = true, length = MAX_LENGTH_NAME)
 	protected @NonNull String name;
-	
+
 	@Column(name = "descriptiom", nullable = true, columnDefinition = "TEXT", length = MAX_LENGTH_DESCRIPTION)
 	protected @Nullable String description;
-	
+
 	@Column(name = "removed_at", nullable = true, insertable = false, updatable = true)
 	protected @Nullable LocalDateTime removedAt;
-	
-	/**
-	 * AllArgsConstructor
-	 * 
-	 * @param entityId
-	 * @param name
-	 * @param description
-	 * @param createdAt
-	 * @param updatedAt
-	 * @param removedAt
-	 */
-	@Builder(builderMethodName = "baseAdminBuilder")
-	@ConstructorProperties({ "entityId", "name", "description", "createdAt", "updatedAt", "removedAt" })
-	public BaseAdmin(final Integer entityId, final String name, final String description, final LocalDateTime createdAt, final LocalDateTime updatedAt, final LocalDateTime removedAt) {
-		this.entityId = entityId;
-		this.name = name;
-		this.description = description;
-		this.createdAt = createdAt;
-		this.updatedAt = updatedAt;
-		this.removedAt = removedAt;
-	}
+
 }
